a0a2843a163d093935c9ed350c5cfb9b69f448ed
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-07 12:38:15 +0300

    Create a euphonious word

--- Problems/Create a euphonious word/src/Task.kt
+++ Problems/Create a euphonious word/src/Task.kt
@@ -1,3 +1,39 @@
+import java.util.*
+
+val vowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
+var vowelsCounter = 0
+var consonantsCounter = 0
+var discordant = 0
+
 fun main() {
-    // write your code here    
+    val scanner = Scanner(System.`in`)
+    val word = scanner.nextLine()
+
+    for (i in 0..word.length - 3) {
+        if (vowels.contains(word[i])) {
+            vowelsCounter++
+        } else {
+            consonantsCounter++
+        }
+        if (vowels.contains(word[i + 1])) {
+            vowelsCounter++
+        } else {
+            consonantsCounter++
+        }
+        if (vowels.contains(word[i + 2])) {
+            vowelsCounter++
+        } else {
+            consonantsCounter++
+        }
+
+        if (vowelsCounter == 3 || consonantsCounter == 3) {
+            discordant++
+            continue
+        }
+
+        vowelsCounter = 0
+        consonantsCounter = 0
+    }
+
+    print(discordant)
 }
