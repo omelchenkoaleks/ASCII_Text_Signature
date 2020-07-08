31af018dd3ac1bed7c374247143007854b4e806e
Omelchenko Aleksandr <omelchenkoaleks@gmail.com>
2020-07-08 13:18:57 +0300

    вот такое решение для генерирования пароля ... )

--- Problems/Generating passwords/src/Task.kt
+++ Problems/Generating passwords/src/Task.kt
@@ -1,28 +1,61 @@
 import java.util.*
 
-val letters = printAlphabet()
 val scanner = Scanner(System.`in`)
-//var uppercaseLetters = 0
-//var lowercaseLetters = 0
+var uppercaseLetters = 0
+var lowercaseLetters = 0
 var digits = 0
 var symbols = 0
-var tmp = ""
+var iterator = 0
+val randomString = mutableListOf<Char>()
 
 fun main() {
-//    uppercaseLetters = scanner.nextInt()
-//    lowercaseLetters = scanner.nextInt()
+    uppercaseLetters = scanner.nextInt()
+    lowercaseLetters = scanner.nextInt()
     digits = scanner.nextInt()
     symbols = scanner.nextInt()
 
-    val randomString = (1..symbols)
-            .map (letters::get)
-            .toCharArray()
+    val allSymbolsFromUser = uppercaseLetters + lowercaseLetters + digits
 
+    if (allSymbolsFromUser <= symbols) {
 
-    print(randomString)
-}
+        repeat(uppercaseLetters) {
+            randomString.add(('A'..'Z').random())
+        }
 
-fun printAlphabet() = buildString {
-    for (letter in 'a'..'z')
-        append(letter)
+        repeat(lowercaseLetters) {
+            randomString.add(('a'..'z').random())
+        }
+
+        repeat(digits) {
+            randomString.add(('0'..'9').random())
+        }
+
+        repeat(symbols - allSymbolsFromUser) {
+            randomString.add(('A'..'Z').random())
+        }
+    }
+
+    for (i in 0..randomString.size - 2) {
+        if (randomString[i] == randomString[i + 1]) {
+            when (randomString[i]) {
+                in 'a'..'z' -> randomString[i] = ('a'..'z').random()
+                in 'A'..'Z' -> randomString[i] = ('A'..'Z').random()
+                in '0'..'9' -> randomString[i] = ('0'..'9').random()
+            }
+        }
+    }
+
+    for (i in 0..randomString.size - 2) {
+        if (randomString[i] == randomString[i + 1]) {
+            when (randomString[i]) {
+                in 'a'..'z' -> randomString[i] = ('a'..'z').random()
+                in 'A'..'Z' -> randomString[i] = ('A'..'Z').random()
+                in '0'..'9' -> randomString[i] = ('0'..'9').random()
+            }
+        }
+    }
+
+    for (ch in randomString) {
+        print("$ch")
+    }
 }
